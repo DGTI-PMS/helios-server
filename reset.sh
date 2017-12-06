4eeb40c591fee2dc8ef1864979bdda39a34ae86c
Jefferson Campos <jecampos@sorocaba.sp.gov.br>
2017-12-06 13:38:37 -0200

    Configurações para o Sistema Funcionar
    Implementada as configurações necessárias para o sistema funcionar. Por questão de segurança, o arquivo settings.py não será salvo neste repositório.

--- reset.sh
+++ reset.sh
@@ -3,4 +3,5 @@ dropdb helios
 createdb helios
 python manage.py syncdb
 python manage.py migrate
-echo "from helios_auth.models import User; User.objects.create(user_type='google',user_id='ben@adida.net', info={'name':'Ben Adida'})" | python manage.py shell
+#echo "from helios_auth.models import User; User.objects.create(user_type='google',user_id='ben@adida.net', info={'name':'Ben Adida'})" | python manage.py shell
+echo "from helios_auth.models import User; User.objects.create(user_type='google',user_id='sorocaba.ti@gmail.com', info={'name':'TI Prefeitura Municipal de Sorocaba'})" | python manage.py shell
